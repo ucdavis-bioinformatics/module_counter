10ecaf63456efdea29bfa907942eb62b1c301125
Nikhil Joshi <najoshi@ucdavis.edu>
2019-11-20 02:12:23 -0800

    collating output from db query

--- test_influxdb2.py
+++ test_influxdb2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import influxdb
-import os
-import datetime
+from collections import Counter
 
 client= influxdb.InfluxDBClient('blammo.genomecenter.ucdavis.edu',8086,'','',"test")
 
 rs=client.query("SELECT * from module_counts;")
 points = list(rs.get_points(measurement='module_counts'))
-print(points)
+#print(points)
 #print(format(rs))
 
+counts = Counter((tok['sw_name'],tok['sw_version']) for tok in points)
+print(counts)
