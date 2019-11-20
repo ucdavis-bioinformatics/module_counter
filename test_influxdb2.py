b4b56ed735e65094117a979b30337c38c1eead57
Nikhil Joshi <najoshi@ucdavis.edu>
2019-11-20 03:05:35 -0800

    sorted output

--- test_influxdb2.py
+++ test_influxdb2.py
@@ -2,7 +2,7 @@
 import influxdb
 from collections import Counter
 
-client= influxdb.InfluxDBClient('blammo.genomecenter.ucdavis.edu',8086,'','',"test")
+client=influxdb.InfluxDBClient('blammo.genomecenter.ucdavis.edu',8086,'','',"test")
 
 rs=client.query("SELECT * from module_counts;")
 points = list(rs.get_points(measurement='module_counts'))
