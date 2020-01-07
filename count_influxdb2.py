beaa4adfa1f6a1d8fc87b1e117f09aea028fd61e
Nikhil Joshi <najoshi@ucdavis.edu>
2020-01-07 01:10:00 -0800

    figuring out anaconda2 bug

--- count_influxdb2.py
+++ count_influxdb2.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 
-import sys
-#print(sys.path)
-# hack for anaconda issues when running script
-try:
-    sys.path.remove('/software/anaconda2/4.5.12/lssc0-linux/lib/python2.7/site-packages')
-    sys.path.remove('/software/anaconda3/4.5.12/lssc0-linux/lib/python3.7/site-packages')
-    sys.path.remove('/software/anaconda3/4.5.12/lssc0-linux/lib/python3.6/site-packages')
-except ValueError:
-    pass
-
-
-import threading
-import time
-import os
-
 def send_count(sw_name,sw_version,user):
     import influxdb
     client = influxdb.InfluxDBClient('blammo.genomecenter.ucdavis.edu',8086,'','',"test")
@@ -41,14 +26,29 @@ def send_count(sw_name,sw_version,user):
 
 
 if __name__ == '__main__':
+    import sys
+    #print(sys.path)
+    # hack for anaconda issues when running script
+    try:
+        sys.path.remove('/software/anaconda2/4.5.12/lssc0-linux/lib/python2.7/site-packages')
+        sys.path.remove('/software/anaconda3/4.5.12/lssc0-linux/lib/python3.7/site-packages')
+        sys.path.remove('/software/anaconda3/4.5.12/lssc0-linux/lib/python3.6/site-packages')
+    except ValueError:
+        pass
     #print(sys.argv[1])
     #print(sys.argv[2])
     #print(sys.argv[3])
     #t = threading.Thread(target=send_count, args=(sys.argv[1],sys.argv[2],sys.argv[3]))
+
+    #print(sys.path)
+
+    import threading
+    import time
+    import os
+
     data = sys.argv[1].split(" ")
     t = threading.Thread(target=send_count, args=(data[0],data[1],data[2]))
     t.daemon = True
     t.start()
     t.join()
-    #print("here")
     os._exit(0)
